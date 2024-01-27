ab4c1cc9a69566600006119f5114636b6dd5a677
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-01-26 20:47:34 -0500

    Terminamos lo de Escribir nombre y de escoger el tipo de player

--- game.rb
+++ game.rb
@@ -6,9 +6,12 @@ def select_character
     valid_characters = characters.keys #[warrrior, magician]
   until valid_characters.include?(character.downcase)
      puts "Choose a character"
-     print_player_characters
+     print_player_characters(valid_characters)
     #  character = gets.chomp.downcase
      character = gets.chomp
+     unless valid_characters.include?(character.downcase)
+        puts "Invalid option !!"
+     end
    end
    character.downcase
   puts "#{character.downcase}" # devuelve el personaje , .downcase: Este método convierte toda la cadena a minúsculas.
@@ -17,12 +20,12 @@ end
 
 
 # Imprime en terminal de manera automatiada lo de posibilidades  de esciger caracteres
-def print_player_characters
-    characters= Catalog::CHARACTERS.select{|name, data| data[:type] == "player"} 
-    names= characters.keys # ["warrior", "magician"]
+def print_player_characters (options) # agregamos parámetros
+    # characters= Catalog::CHARACTERS.select{|name, data| data[:type] == "player"} # eliminamos esto porque se repite "elect_character"
+    # names= characters.keys # ["warrior", "magician"] # eliminamos esto porque se repite "elect_character"
     # pp characters
-    names.each_with_index do |name , index|
-        puts "#{index +1}. #{name.capitalize}" #.capitalize: Este método convierte el primer carácter de una cadena a mayúsculas y el resto de la cadena a minúsculas.
+    options.each_with_index do |option , index|
+        puts "#{index +1}. #{option.capitalize}" #.capitalize: Este método convierte el primer carácter de una cadena a mayúsculas y el resto de la cadena a minúsculas.
     end
      print "> "
 end
