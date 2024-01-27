df7aefcf705f844e6d0e1883edaf6d280c26e578
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-01-27 00:16:23 -0500

    seguimos

--- game.rb
+++ game.rb
@@ -1,13 +1,13 @@
 require_relative "catalog/characters" # forma para importar el archivo en ruby characters.rb desde la carpeta catalog
  # Automatiaciones de los empty?                     
-#  def get_input(prompt)
-#  input = ""
-#    while input.empty?
-#    puts prompt
-#    print "> "
-#    input = gets.chomp 
-#    end
-#  end
+ def get_input(prompt)
+   input = ""
+   while input.empty?
+     puts prompt
+     print "> "
+     input = gets.chomp 
+   end
+ end
 
 # Definene Caracter válido
 def select_character
@@ -30,39 +30,17 @@ end
 
 
 # Imprime en terminal de manera automatiada lo de posibilidades  de esciger caracteres
-def print_player_characters (options) # agregamos parámetros
-    # characters= Catalog::CHARACTERS.select{|name, data| data[:type] == "player"} # eliminamos esto porque se repite "elect_character"
-    # names= characters.keys # ["warrior", "magician"] # eliminamos esto porque se repite "elect_character"
-    # pp characters
+def print_player_characters(options) # agregamos parámetros
+   
     options.each_with_index do |option , index|
         puts "#{index +1}. #{option.capitalize}" #.capitalize: Este método convierte el primer carácter de una cadena a mayúsculas y el resto de la cadena a minúsculas.
     end
      print "> "
 end
 
-######Part1 
-name = ""
-while name.empty?
-   puts "What's your name"
-   print "> "
-   name = gets.chomp 
-  #puts "Imput: #{name}"  
-end
-######Part2 2
-
-character= select_character
-# puts Catalog::CHARACTERS  # verificamos si importamos bien
-# pp Catalog::CHARACTERS   # verificamos si importamos bien y si lo quieres más ordenado
-#filtered_hash = hash.select { |key, value| value >2}
-
-
-#Forma rudimentaria de programar
-character_name = ""
-while character_name.empty?
-  puts "Give your character a name:"
-  print "> "
-  character_name = gets.chomp   
-end
+name = get_input("What's your name") 
+character = select_character
+character_name = get_input("Give your character a name:") 
 puts "Name: #{name}"
 puts "Character type: #{character}"
 puts "Character name: #{character_name}"
