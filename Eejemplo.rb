851ec611b5c7afbf77f9e22d14e82eaca2783aeb
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-01-28 11:28:07 -0500

    añadiendo a la clase Character más llamadas de variables

--- Eejemplo.rb
+++ Eejemplo.rb
@@ -1,21 +1,69 @@
 
-character = ""
-while character.empty?
-    puts "Choose a character"
-    #    puts "1. warrior"
-    #    puts "2. magican"
-    puts "1. warrior"
-    puts "2. magician"
-#    print_player_characters
-     character = gets.chomp
-     if character == "" or character.upcase !="WARRIOR" and character.upcase != "MAGICIAN"  then
-        puts "Invalid character, try again"
-        character =""
-        next
-     else 
-        break
-    end
-end
-puts character
+# character = ""
+# while character.empty?
+#     puts "Choose a character"
+#     #    puts "1. warrior"
+#     #    puts "2. magican"
+#     puts "1. warrior"
+#     puts "2. magician"
+# #    print_player_characters
+#      character = gets.chomp
+#      if character == "" or character.upcase !="WARRIOR" and character.upcase != "MAGICIAN"  then
+#         puts "Invalid character, try again"
+#         character =""
+#         next
+#      else 
+#         break
+#     end
+# end
+# puts character
 
 #  if character == "\n" or character.upper != "WARRIOR" and character.upper != "MAGICIAN"  then
+# class Character
+#     def initialize(name, type)
+#       @name = name
+#       @character = type
+#     end
+#   end
+
+
+class Persona
+    # attr_accessor :nombre, :edad
+  
+    def initialize(nombre, edad)
+      @nombre = nombre
+      @edad = edad
+    end
+  
+    # def saludar
+    #   puts "Hola, soy #{nombre} y tengo #{edad} años."
+    # end
+  end
+  
+  # Crear una instancia de la clase Persona
+  persona1 = Persona.new("Juan", 25)
+  
+  # Llamar al método saludar de la instancia
+#   persona1.saludar
+  
+p  persona1
+
+
+
+
+#   class Player
+#     def initialize(name, character_type, characte_name)
+#       @name = name
+#       @character = Character.new(characte_name,character_type)
+#     end
+#   end
+  
+#   class Character
+#     def initialize(name, type)
+#       @name = name
+#       @character = type
+#     end
+#   end
+  
+#   player = Player.new("Axel", "Warior", "Poder")
+#   p player
--- game.rb
+++ game.rb
@@ -14,6 +14,60 @@ end
 name = get_input("What's your name?")
 character = select_character
 character_name = get_input("Give your character a name:")
-puts "Name: #{name}"
-puts "Character type: #{character}"
-puts "Character name: #{character_name}"
+# Emepzamos a automatiar los tres "puts" de abajo" con clases. Esto nos va a servir para otras peticiones del ejerciocio por lo que podemos reutilizarlos
+# puts "Name: #{name}"
+# puts "Character type: #{character}"
+# puts "Character name: #{character_name}"
+class Player #yo como jugador
+  def initialize(name, character_type, character_name)
+    @name = name
+    @character = Character.new(character_name,character_type)
+  end
+end
+
+class Bot <  Player # como bot
+  def initialize #(name, character_type, character_name)
+    bot_characteres= Catalog::CHARACTERS.select{|name, data| data[:type] == "bot"}
+    options = bot_characteres.keys #["lockhead", "Dragon", ..]
+    select_character = options.sample
+    super("Dragon Master", select_character, select_character.capitalize)
+  end
+end
+
+class Character # automatizado para extraer datos tanto del bot y player 
+  def initialize(name, type)
+    char_details= Catalog::CHARACTERS[type] # es type sin : porque es un string
+    @name = name
+    @type = type
+    @experience= char_details[:base_exp]
+    @max_health= char_details[:base_stats][:hp]
+    @speed = char_details[:base_stats][:speed]
+    @moves= char_details[:moves]
+    @health= nil
+    @current_move= nil
+  end
+end
+
+# def prepare_to_battle
+#   @health = @max_health
+#   @curent_move = nill
+# end 
+
+
+# def receive_damage(damage)
+#   @health = @max_health
+#   @curent_move = nill
+# end 
+
+
+
+# def prepare_to_battle
+#   @health = @max_health
+#   @curent_move = nill
+# end 
+
+
+player = Player.new(name, character, character_name)
+p player
+bot = Bot.new
+p bot
